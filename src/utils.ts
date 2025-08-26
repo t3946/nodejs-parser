c7daa47a75a0f37e17e42abcb7fb47132d624e6c
Vladislav <svialence@yandex.ru>
2025-08-26 19:47:20 +0300

    remove unused

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,5 @@
 import axios from "axios";
 
-export const fetchProxies = async (): Promise<string[]> => {
-    const resp = await fetch("https://api.proxytraff.com/package/get?c=K5hk");
-
-    return (await resp.text()).split('\n');
-}
-
 export const sleep = (timeMS: number) => new Promise(resolve => setTimeout(resolve, timeMS));
 
 export async function linkToBase64(url: string): Promise<string> {
